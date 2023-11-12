11c9a658ec239863ddb63b88a89d14661e6d1a25
ethan7lesar <ethanlesar11@gmail.com>
2023-11-12 20:01:20 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Projects from '../pages/Projects';
+import Contact from '../pages/Contact';
+
+export {Home, About, Projects, Contact};
